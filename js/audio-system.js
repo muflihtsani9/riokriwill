137d81b8fef03662691ce2c04f76fb9e9c22d974
fatintsani <fatin.tsani231@gmail.com>
2026-05-24 19:35:18 +0700

    first commit

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Sampai Jadi Debu',
-    artist:    'Banda Neira',
+    title:     ' rearranged my world',
+    artist:    'daniel caesar',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
